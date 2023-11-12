6f43149e81d4355186b0a9eb584e8474053b609d
fkrydev <fikri.afa289@gmail.com>
2023-11-12 10:35:39 +0800

    adding preloading page

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,6 +16,54 @@
 </head>
 
 <body x-data="{ open: false }" class="bg-slate-100">
+    <div class="preloader">
+        <style>
+            .loader {
+                opacity: 1;
+                transition: all 1000ms;
+                top: 0%
+            }
+
+            .hide {
+                opacity: 0;
+
+            }
+
+            .top {
+                display: none;
+            }
+        </style>
+        <div class="loader fixed bg-white w-full h-screen z-[500]" id="loader">
+            <div class="flex justify-center items-center h-screen">
+                <div class="flex flex-col items-center mx-4">
+                    <img src="{{ asset('assets/image/logo.png') }}" alt="logo" class="lg:w-[200px] w-[150px]">
+                    <h1 id="animated-word" class="mt-3 lg:text-3xl text-xl font-semibold uppercase text-center"></h1>
+                </div>
+            </div>
+        </div>
+        <script>
+            const word = "Portal Akademik Dosen";
+            const animatedWordElement = document.getElementById("animated-word");
+
+            function animateText(index) {
+                if (index < word.length) {
+                    animatedWordElement.textContent += word[index];
+                    index++;
+                    setTimeout(() => animateText(index), 40)
+                }
+            }
+
+            window.onload = function() {
+                animateText(0)
+                setTimeout(() => {
+                    document.getElementById('loader').classList.add('hide');
+                    setTimeout(() => {
+                        document.getElementById('loader').classList.add('top');
+                    }, 1200)
+                }, 1200);
+            }
+        </script>
+    </div>
     <div class="flex lg:gap-3 w-full relative ">
         <div class="lg:basis-[13%] relative flex-grow">
             <x-Sidebar />
